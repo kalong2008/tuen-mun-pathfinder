46fe1e836f16ef207b7c51a84f64badd0b907287
Chan Ka Long <longchan05@gmail.com>
2025-01-31 17:23:00 +0800

    ADD

---
+++ public/calendar-date.tsx
@@ -0,0 +1,16 @@
+export const activityDateDetail = [
+    { date: "72024", datedetail: "720241", name: "7月06日：集會", date_format: new Date("2024-07-06") },
+    { date: "72024", datedetail: "720241", name: "7月06日：集會", date_format: new Date("2024-07-06") },
+    { date: "72024", datedetail: "720242", name: "7月13日：集會", date_format: new Date("2024-07-13") },
+    { date: "72024", datedetail: "720243", name: "7月20日：集會", date_format: new Date("2024-07-20") },
+    { date: "72024", datedetail: "720244", name: "7月27日：集會", date_format: new Date("2024-07-27") },
+    { date: "72024", datedetail: "720241", name: "7月06日：集會", date_format: new Date("2024-07-06") },
+  
+    { date: "82024", datedetail: "820241", name: "ee", date_format: new Date("2024-08-03") },
+    { date: "82024", datedetail: "820242", name: "ff", date_format: new Date("2024-08-10") },
+    { date: "82024", datedetail: "820243", name: "gg", date_format: new Date("2024-08-17") },
+    { date: "82024", datedetail: "820244", name: "hh", date_format: new Date("2024-08-24") },
+    { date: "82024", datedetail: "820244", name: "hh", date_format: new Date("2024-08-31") },
+  ];
+  
+  export const highlightedDates = activityDateDetail.map(function(dates) {return dates.date_format});
---
+++ public/hyperlink-data.tsx
@@ -0,0 +1,438 @@
+export const hyperLink2011 = [
+    {
+      name: "2011年07月露營",
+      href: "/2011-07-camp",
+    },
+  ];
+
+  export const hyperLink2012 = [
+    {
+      name: "2012年03月大匯操",
+      href: "/2012-03-pathfinder-rally",
+    },
+    {
+      name: "2012年04月露營",
+      href: "/2012-04-camp",
+    },
+    {
+      name: "2012年07月運動營",
+      href: "/2012-07-camp",
+    },
+    {
+      name: "2012年08月升級禮",
+      href: "/2012-08-promotion",
+    },
+    {
+      name: "2012年11月遠足",
+      href: "/2012-11-hiking",
+    },
+    {
+      name: "2012年12月金波利",
+      href: "/2012-12-camporee",
+    },
+  ];
+
+  export const hyperLink2013 = [
+    {
+      name: "2013年03月大匯操",
+      href: "/2013-03-pathfinder-rally",
+    },
+    {
+      name: "2013年04月露營",
+      href: "/2013-04-camp",
+    },
+    {
+      name: "2013年08月升級禮",
+      href: "/2013-08-promotion",
+    },
+    {
+      name: "2013年08月運動營",
+      href: "/2013-08-camp",
+    },
+    {
+      name: "2013年11月遠足",
+      href: "/2013-11-hiking",
+    },
+    {
+      name: "2013年12月金波利",
+      href: "/2013-12-camporee",
+    },
+  ];
+
+  export const hyperLink2014 = [
+    {
+      name: "2014年03月大匯操",
+      href: "/2014-03-pathfinder-rally",
+    },
+    {
+      name: "2014年03月遠足",
+      href: "/2014-03-hiking",
+    },
+    {
+      name: "2014年04月露營",
+      href: "/2014-04-camp",
+    },
+    {
+      name: "2014年07月嘉年華",
+      href: "/2014-07-booth",
+    },
+    {
+      name: "2014年08月升級禮",
+      href: "/2014-08-promotion",
+    },
+    {
+      name: "2014年08月生死教育",
+      href: "/2014-08-life-and-death",
+    },
+    {
+      name: "2014年09月運動日",
+      href: "/2014-09-day-camp",
+    },
+    {
+      name: "2014年10月浸禮",
+      href: "/2014-10-baptist",
+    },
+    {
+      name: "2014年10月遠足",
+      href: "/2014-10-hiking",
+    },
+    {
+      name: "2014年11月濕地公園",
+      href: "/2014-11-wetland-park",
+    },
+    {
+      name: "2014年11月戶外活動",
+      href: "/2014-11-outing",
+    },
+    {
+      name: "2014年12月金波利",
+      href: "/2014-12-camporee",
+    },
+  ];
+
+  export const hyperLink2015 = [
+    {
+      name: "2015年03月大匯操",
+      href: "/2015-03-pathfinder-rally",
+    },
+    {
+      name: "2015年04月福音營",
+      href: "/2015-04-camp",
+    },
+    {
+      name: "2015年05月遠足",
+      href: "/2015-05-hiking",
+    },
+    {
+      name: "2015年05月露營",
+      href: "/2015-05-camp",
+    },
+    {
+      name: "2015年06月遠足",
+      href: "/2015-06-hiking",
+    },
+    {
+      name: "2015年08月露營",
+      href: "/2015-08-camp",
+    },
+    {
+      name: "2015年09月聯合升級禮",
+      href: "/2015-09-promotion",
+    },
+    {
+      name: "2015年11月遠足",
+      href: "/2015-11-hiking",
+    },
+    {
+      name: "2015年12月金波利",
+      href: "/2015-12-camporee",
+    },
+  ];
+
+  export const hyperLink2016 = [
+    {
+      name: "2016年03月露營",
+      href: "/2016-03-camp",
+    },
+    {
+      name: "2016年04月露營",
+      href: "/2016-04-camp",
+    },
+    {
+      name: "2016年05月遠足",
+      href: "/2016-05-hiking",
+    },
+    {
+      name: "2016年07月露營",
+      href: "/2016-07-camp",
+    },
+    {
+      name: "2016年09月升級禮",
+      href: "/2016-09-promotion",
+    },
+    {
+      name: "2016年10月前鋒會日",
+      href: "/2016-10-pathfinder-day",
+    },
+    {
+      name: "2016年11月嘉道理農場",
+      href: "/2016-11-visit",
+    },
+    {
+      name: "2016年12月行山",
+      href: "/2016-12-hiking",
+    },
+    {
+      name: "2016年12月金波利",
+      href: "/2016-12-camporee",
+    },
+  ];
+
+  export const hyperLink2017 = [
+    {
+      name: "2017年01月遠足",
+      href: "/2017-01-hiking",
+    },
+    {
+      name: "2017年04月露營",
+      href: "/2017-04-camp",
+    },
+    {
+      name: "2017年05月露營",
+      href: "/2017-05-camp",
+    },
+    {
+      name: "2017年07月宿營",
+      href: "/2017-07-camp",
+    },
+    {
+      name: "2017年08月北亞太金波利",
+      href: "/2017-08-nsd-camporee",
+    },
+    {
+      name: "2017年08月升級禮",
+      href: "/2017-08-promotion",
+    },
+    {
+      name: "2017年10月童Sing頌讚Open Day",
+      href: "/2017-10-open-day",
+    },
+    {
+      name: "2017年11月遠足",
+      href: "/2017-11-hiking",
+    },
+    {
+      name: "2017年12月宿營",
+      href: "/2017-12-camp",
+    },
+    {
+      name: "2017年暑期活動",
+      href: "/2017-summer-activity",
+    },
+    {
+      name: "2017年11月親子運動日",
+      href: "/2017-11-sport-day",
+    },
+
+
+
+    
+  ];
+
+  export const hyperLink2018 = [
+    {
+      name: "2018年01月遠足",
+      href: "/2018-01-hiking",
+    },
+    {
+      name: "2018年03月露營",
+      href: "/2018-03-camp",
+    },
+    {
+      name: "2018年04月露營",
+      href: "/2018-04-camp",
+    },
+    {
+      name: "2018年05月螢火蟲探險",
+      href: "/2018-05-hiking",
+    },
+    {
+      name: "2018年05月前鋒會日",
+      href: "/2018-05-pathfinder-day",
+    },
+    {
+      name: "2018年06月前鋒會日",
+      href: "/2018-06-promotion-camp",
+    },
+    {
+      name: "2018年08月暑期活動",
+      href: "/2018-08-summer-activity",
+    },
+    {
+      name: "2018年07月升級禮",
+      href: "/2018-07-promotion",
+    },
+    {
+      name: "2018年08月我懂我Mood親子營",
+      href: "/2018-08-camp",
+    },
+    {
+      name: "2018年09月露營",
+      href: "/2018-09-camp",
+    },
+    {
+      name: "2018年11月遠足",
+      href: "/2018-11-hiking",
+    },
+    {
+      name: "2018年12月金波利大會",
+      href: "/2018-12-camporee",
+    }
+  ];
+
+
+
+  export const hyperLink2019 = [
+    {
+      name: "2019年04月露營",
+      href: "/2019-04-camp",
+    },
+    {
+      name: "2019年05月前鋒會日",
+      href: "/2019-05-pathfinder-day",
+    },
+    {
+      name: "2019年05月母親節活動",
+      href: "/2019-05-mother-day",
+    },
+    {
+      name: "2019年06月小「煮」人",
+      href: "/2019-06-cooking",
+    },
+    {
+      name: "2019年06月露營",
+      href: "/2019-06-camp",
+    },
+    {
+      name: "2019年07月升級營",
+      href: "/2019-07-promotion-camp",
+    },
+    {
+      name: "2019年07月國際金波利",
+      href: "/2019-07-international-camporee",
+    },
+    {
+      name: "2019年08月暑期烹飪班",
+      href: "/2019-08-cooking",
+    },
+    {
+      name: "2019年09月冰皮月餅",
+      href: "/2019-09-moon-cake",
+    },
+    {
+      name: "2019年09月升級禮",
+      href: "/2019-09-promotion",
+    },
+    {
+      name: "2019年11月露營",
+      href: "/2019-11-camp",
+    },
+    {
+      name: "2019年12月金波利",
+      href: "/2019-12-camporee",
+    }
+  ];
+
+  export const hyperLink2020 = [
+    {
+      name: "2020年09月攝影榮譽證",
+      href: "/2020-09-photograph-honor",
+    },
+    {
+      name: "2020年10月陶瓷榮譽證",
+      href: "/2020-10-ceramic-honor",
+    },
+    {
+      name: "2020年11月陶瓷榮譽證",
+      href: "/2020-11-ceramic-honor",
+    },
+  ];
+
+  export const hyperLink2021 = [
+    {
+      name: "2021年10月陶瓷榮譽證",
+      href: "/2021-10-ceramic-honor",
+    },
+    {
+      name: "2021年07月暑期活動",
+      href: "/2021-07-summer-activity",
+    },
+  ];
+
+  export const hyperLink2022 = [
+    {
+      name: "2022年升級訓練",
+      href: "/2022-promotion-training",
+    },
+    {
+      name: "2022年06月遠足",
+      href: "/2022-06-hiking",
+    },
+    {
+      name: "2022年06月嘉道理農場",
+      href: "/2022-06-outing",
+    },
+    {
+      name: "2022年07月太空館",
+      href: "/2022-07-space-museum",
+    },
+    {
+      name: "2022年07月合味道參觀",
+      href: "/2022-07-visit",
+    },
+    {
+      name: "2022年08月升級禮",
+      href: "/2022-08-promotion",
+    },
+    {
+      name: "2022年12月報佳音",
+      href: "/2022-12-christmax",
+    },
+  ];
+
+  export const hyperLink2023 = [
+    {
+      name: "2023年04月前鋒露營",
+      href: "/2023-04-pathfinder-camp",
+    },
+    {
+      name: "2023年04月幼鋒露營",
+      href: "/2023-04-adventurer-camp",
+    },
+    {
+      name: "2023年05月遠足",
+      href: "/2023-05-hiking",
+    },
+    {
+      name: "2023年08月宿營",
+      href: "/2023-08-camp",
+    },
+    {
+      name: "2023年08月烹飪",
+      href: "/2023-08-cooking",
+    },
+  ];
+
+  export const hyperLink2024 = [
+    {
+      name: "Nil",
+      href: "/#",
+    },
+  ];
+
+  export const hyperLinkOther = [
+    {
+      name: "2024年07月國際金波利",
+      href: "/2024-07-international-camporee",
+    },
+  ];
