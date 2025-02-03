60fa26a89c78a3c5a9767cf0c4067be2424b1e6a
Chan Ka Long <longchan05@gmail.com>
2025-02-03 22:23:42 +0800

    add photo and icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,13 @@ import { Noto_Sans_HK } from "next/font/google";
 import "./globals.css";
 import SideNav from "@/app/ui/nagivation";
 import FooterComponent from "./ui/footer";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const notoHK = Noto_Sans_HK({ preload: false, });
 
 export const metadata: Metadata = {
   title: "屯門前鋒會 幼鋒會 | 基督復臨安息日會屯門教會",
-  description: "tuen mun pathfinder and adventurer",
+  description: "屯門前鋒會 幼鋒會 | 基督復臨安息日會屯門教會",
 };
 
 export default function RootLayout({
