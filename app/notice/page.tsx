301d3213414023cf1e957fafc1686ce5d7079cee
Chan Ka Long <longchan05@gmail.com>
2025-03-27 09:06:14 +0800

    add notice

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -5,7 +5,10 @@ export default function Page() {
   return (
     <div className="w-4/5 m-auto pb-14">
       <h1 className="md:text-3xl font-bold leading-6 text-gray-900 text-center md:py-10 text-xl py-4">2025年通告下載</h1>
-      <Link href="/notice/2025-2-2 hiking.pdf" className="leading-6 text-center underline" target="_blank">2月2日遠足</Link>
+      <div className="flex flex-col"><Link href="/notice/2025-4-4_camping-adventurer.pdf" className="leading-6 text-center underline" target="_blank">4月4-6日露營（幼鋒會）</Link>
+      <Link href="/notice/2025-4-4_camping-pathfinder.pdf" className="leading-6 text-center underline" target="_blank">4月4-6日露營（前鋒會）</Link>
+      <Link href="/notice/2025-4-12-cupnoodle.pdf" className="leading-6 text-center underline" target="_blank">4月12日參觀合味道紀念館</Link></div>
+      
     </div>
   );
   
