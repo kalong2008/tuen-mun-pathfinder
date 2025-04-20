3a187f5f3ccfeaa6a76236273bf2ad10bcf896ca
Chan Ka Long <longchan05@gmail.com>
2025-04-20 12:35:04 +0800

    add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,20 @@ const notoHK = Noto_Sans_HK({ preload: false, });
 
 export const metadata: Metadata = {
   title: "屯門前鋒會 幼鋒會 | 基督復臨安息日會屯門教會",
-  description: "屯門前鋒會 幼鋒會 | 基督復臨安息日會屯門教會",
+  description: "屯門前鋒會及幼鋒會(6-15歲)提供信仰為本的全人發展培訓。透過歷奇、服務與技能學習，促進兒童及青少年在品格、領導力、生活技能、愛心，以至情緒管理方面的成長。歡迎加入！",
+  openGraph: {
+    title: '屯門前鋒會 幼鋒會 | 基督復臨安息日會屯門教會',
+    description: '屯門前鋒會及幼鋒會(6-15歲)提供信仰為本的全人發展培訓。透過歷奇、服務與技能學習，促進兒童及青少年在品格、領導力、生活技能、愛心，以至情緒管理方面的成長。歡迎加入！',
+    url: 'https://' + process.env.VERCEL_PROJECT_PRODUCTION_URL,
+    images: [
+      {
+        url: 'https://' + process.env.VERCEL_PROJECT_PRODUCTION_URL + '/photo/2024/2024-09-promotion/2024-09-promotion-87.jpeg',
+        width: 4000,
+        height: 1610,
+        alt: '屯門前鋒會 幼鋒會 | 基督復臨安息日會屯門教會',
+      },
+    ],
+  }
 };
 
 export default function RootLayout({
